0bdc8beb260680b67507a9ca258f2edd8f9a8a99
Nguyễn Hữu Phước <59969725+TheCoffeeGuyy@users.noreply.github.com>
2021-10-14 09:22:02 +0700

    update octaves

--- BackTracking/Octaves.py
+++ BackTracking/Octaves.py
@@ -1,22 +1,25 @@
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
-t = int(input())
-
-def backtrack (a, r, c, visited):
-    if r >= n or c >= n or visited[r][c] or r < 0 or c < 0 or matrix[r][c] == '.':
-        return
-    a.append((r, c))
+def backtrack (a, r, c):
+    a.append(r * n + c)
     visited[r][c] = True
     if len(a) == 8:
-        print(a)
+        a.sort()
+        cleanAns = ''
+        for el in a:
+            cleanAns += str(el)
+        ans.add(cleanAns)
         return
     for k in range(4):
         tempr = dx[k] + r
         tempc = dy[k] + c
-        backtrack(a, tempr, tempc, visited)
+        if tempr in range(n) and tempc in range(n) and not visited[tempr][tempc] and matrix[tempr][tempc] != '.':
+            backtrack(a, tempr, tempc)
     visited[r][c] = False
     a.pop()
+
+t = int(input())
 
 for _ in range(t):
     n = int(input())
@@ -28,8 +31,11 @@ for _ in range(t):
     
     visited = [[False for i in range(20)] for j in range(20)]
 
-    ans = []
-    a = []
+    ans = set()
+    
     for i in range(n):
         for j in range(n):
-            backtrack(a, i, j, visited)
+            if matrix[i][j] != '.':
+                a = []
+                backtrack(a, i, j)
+    print(len(ans))
